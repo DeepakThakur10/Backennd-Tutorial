199b6d4ed07cf60a4f6c0c454992060671cef64d
DeepakThakur10 <dk1816859@gmail,com>
2026-02-23 15:21:15 +0530

    Add MongoDB connection logic and update dependencies in package files

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express=require("express");
-
+const dotenv=require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
 
+dotenv.config();
+/*const MongoDB = require("./Config/db.js")
+MongoDB();*/
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("MOngoDB Connect Successfully");
+})
+.catch(()=>{
+    console.log("MongoDB Connection Failed")
+})
 
 app.use(express.json());
 
+
 const userRoutes = require("./routes/userRoutes.js");
+
 
 app.use("/api/users",userRoutes);
 
